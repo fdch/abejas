0ab88d3c19cb823d74e30c1d2ed57c33ff48f069
Fede Camara Halac <camarafede@gmail.com>
2023-01-31 17:02:52 +0100

    updates

--- bees.py
+++ bees.py
@@ -1,33 +1,17 @@
-import numpy as np
-import copy, logging
-from time import sleep
-from .socket import send_to_socket
 from pythonosc import udp_client
+from bees_algorithm import BeesAlgorithm
+import benchmark_functions as bf
 
-class BeesAlgorithm3D(BeesAlgorithm):
-    def __init__(self, score_function, range_min, range_max, **kwargs):
-        super().__init__(score_function, range_min, range_max, **kwargs)
-        self.client = udp_client.SimpleUDPClient("localhost","3003")
+client = udp_client.SimpleUDPClient("localhost",3003)
 
+b_func = bf.Schwefel(opposite=True, n_dimensions=3)
 
-    def compute_and_send(self):
-        self.keep_bees_trace = True
+suggested_lowerbound, suggested_upperbound = b_func.suggested_bounds()
 
-        iteration = 0
+schwefel_bees_parameters = {'ns':0, 'nb':14, 'ne':1, 'nrb':5, 'nre':30, 'stlim':10}
 
-        while True:
-            iteration += 1
-            self.performSingleStep()
-            points_x, points_y, points_z = [], [], []
-            for b in self.to_save_best_sites:
-                points_x += [b.values[0]]
-                points_y += [b.values[1]]
-                points_z += [b.values[2]]
-                for fs in self.to_save_foragers:
-                    for f in fs:
-                        points_x += [f.values[0]]
-                        points_y += [f.values[1]]
-                        points_z += [f.values[2]]
+alg = BeesAlgorithm(b_func, suggested_lowerbound, suggested_upperbound, **schwefel_bees_parameters)
 
-            for i, (x,y,z) in enumerate(zip(points_x, points_y, points_z)):
-                self.client.send_message("/bee", i,x,y,z)
+while True:
+    alg.performSingleStep()
+    client.send_message("/bee", map(lambda x:str(x/500*15),alg.current_sites[-1].values))
