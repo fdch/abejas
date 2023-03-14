6b720a2e9b0a106ddbf2d9e799142f56a6fc0827
Fede Camara Halac <camarafede@gmail.com>
2023-03-14 18:33:20 +0100

    fix osc

--- bees.py
+++ bees.py
@@ -2,7 +2,7 @@ from pythonosc import udp_client
 from bees_algorithm import BeesAlgorithm
 import benchmark_functions as bf
 
-client = udp_client.SimpleUDPClient("localhost",3003)
+client = udp_client.SimpleUDPClient("127.0.0.1",3003)
 
 b_func = bf.Schwefel(opposite=True, n_dimensions=3)
 
@@ -14,4 +14,9 @@ alg = BeesAlgorithm(b_func, suggested_lowerbound, suggested_upperbound, **schwef
 
 while True:
     alg.performSingleStep()
-    client.send_message("/bee", map(lambda x:str(x/500*15),alg.current_sites[-1].values))
+    print(alg.current_sites[-1])
+    x, y, z = alg.current_sites[-1].values
+    client.send_message("/RoomEncoder/sourceX", str(x/500*15))
+    client.send_message("/RoomEncoder/sourceY", str(y/500*15))
+    client.send_message("/RoomEncoder/sourceZ", str(z/500*10))
+    #client.send_message("/bee", map(lambda x:str(x/500*15),alg.current_sites[-1].values))
